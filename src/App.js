a6f8f1beb914629ebc0810c2107102f1750d2587
let <kyle.tran.le@gmail.com>
2022-09-26 11:28:42 -0700

    bomb and treasure

--- src/App.js
+++ src/App.js
@@ -15,25 +15,38 @@ const App = () => {
     "?"
   ])
 
+  const [treasureLocation, setTreasureLocation] = useState(Math.floor(Math.random() * board.length))
+  const [bombLocation, setBombLocation] = useState(Math.floor(Math.random() * board.length))
+
   const handleGamePlay = (clickedSquare) => {
-    let updatedBoard = [...board]
-    updatedBoard[clickedSquare] = "♛"
-    setBoard(updatedBoard)
+    // makes a copy of the board in state so  that we can modify it in this method
+    let updateBoard = [...board]
+    if(clickedSquare === treasureLocation) {
+      updateBoard[clickedSquare] = "💎"
+      setBoard(updateBoard)
+    } else if(clickedSquare === bombLocation) {
+      updateBoard[clickedSquare] = "💣"
+      setBoard(updateBoard)
+    } else {
+      updateBoard[clickedSquare] = "🌴"
+    setBoard(updateBoard)
+    }
   }
 
+  console.log("treasureLocation:", treasureLocation)
+  console.log("bombLocation:", bombLocation)
   return (
     <>
       <h1>Treasure Hunt Game</h1>
       <div className="board-game">
         {board.map((square, index) => {
           return (
-            <Square
-              square={square}
-              index={index}
-              key={index}
-              handleGamePlay={handleGamePlay}
-            />
-          )
+          <Square 
+          square={square} 
+          index={index} 
+          key={index}
+          handleGamePlay={handleGamePlay} />
+        )
         })}
       </div>
     </>
